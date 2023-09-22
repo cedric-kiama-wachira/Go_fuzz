229debcdcd8ab071d740867ea4629893ef4005a2
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-22 18:59:14 +0400

    Here I will debug the double reverse failure and fix the bug. Firstly on looking closely at the reversed string to spot the error. In Go, a string is a read only slice of bytes, and can contain bytes that aren’t valid UTF-8. The original string is a byte slice with one byte, \xe5. When the input string is set to []rune, Go encodes the byte slice to UTF-8, and replaces the byte with the UTF-8 character �. When I compare the replacement UTF-8 character to the input byte slice, they are clearly not equal.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@
 // 2. Wrote the package declaration.
 // 3. Included main function to initialize a string, reverse it, print the output, and repeat. 
 // 4. The main function uses the fmt package, so I will need to import it.
+// 5. Replacing the Reverse function so that we can traverse the string by runes, instead of by bytes.
+
 
 // Step 1.
 package main
@@ -22,12 +24,20 @@ func main() {
 
 
 // Step 2.
+//func Reverse(s string) string {
+//    b := []byte(s)
+//    for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+//        b[i], b[j] = b[j], b[i]
+//    }
+//    return string(b)
+//}
+
+// Step 5.
 func Reverse(s string) string {
-    b := []byte(s)
-    for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
-        b[i], b[j] = b[j], b[i]
+    r := []rune(s)
+    for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+        r[i], r[j] = r[j], r[i]
     }
-    return string(b)
-}
-	
+    return string(r)
+}	
 
