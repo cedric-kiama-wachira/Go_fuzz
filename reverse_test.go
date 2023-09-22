86a87331eede166c123d12c702365ef813660d49
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-22 18:14:38 +0400

    Fuzzing has a few limitations as well. In my unit test, I can predict the expected output of the Reverse function, and verify that the actual output met those expectations.For example, in the test case Reverse HELLO WORLD the unit test specifies the return as DLROW ,OLLEH. With fuzzing, I or anyone else running the test code can’t predict the expected output, since we don’t have control over the inputs. However, there are a few properties of the Reverse function that I can verify in a fuzz test. The two properties being checked in this fuzz test are: One, Reversing a string twice preserves the original value, and two, The reversed string preserves its state as valid UTF-8.The function begins with FuzzXxx instead of TestXxx, and takes *testing.F instead of *testing.T. Where I would expect to see a t.Run execution, instead I see f.Fuzz which takes a fuzz target function whose parameters are *testing.T and the types to be fuzzed. The inputs from my unit test are provided as seed corpus inputs using f.Add. The code comments give the exact explanations

--- reverse_test.go
+++ reverse_test.go
@@ -1,7 +1,10 @@
 // Steps:
 // 1. Declared the main testing package name
 // 2. Imported the library for use
-// 3. Wrote the test function
+// 3. Wrote the static unit test function
+// 4. Commented out the function in step 3 and added the fuzz test
+// 5.
+// 6.
 
 // Step 1.
 package main
@@ -12,18 +15,36 @@ import (
 )
 
 // Step 3.
-func TestReverse(t *testing.T) {
-    testcases := []struct {
-        in, want string
-    }{
-        {"Hello, world", "dlrow ,olleH"},
-        {" ", " "},
-        {"!12345", "54321!"},
-    }
+// func TestReverse(t *testing.T) {
+//    testcases := []struct {
+//        in, want string
+//    }{
+//        {"Hello, world", "dlrow ,olleH"},
+//        {" ", " "},
+//        {"!12345", "54321!"},
+//    }
+//    for _, tc := range testcases {
+//        rev := Reverse(tc.in)
+//        if rev != tc.want {
+//                t.Errorf("Reverse: %q, want %q", rev, tc.want)
+//       }
+//    }
+//}
+
+// Step 4.
+func FuzzReverse(f *testing.F) {
+    testcases := []string{"Hello, world", " ", "!12345"}
     for _, tc := range testcases {
-        rev := Reverse(tc.in)
-        if rev != tc.want {
-                t.Errorf("Reverse: %q, want %q", rev, tc.want)
-        }
+        f.Add(tc)  // Use f.Add to provide a seed corpus
     }
+    f.Fuzz(func(t *testing.T, orig string) {
+        rev := Reverse(orig)
+        doubleRev := Reverse(rev)
+        if orig != doubleRev {
+            t.Errorf("Before: %q, after: %q", orig, doubleRev)
+        }
+        if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+            t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+        }
+    })
 }
